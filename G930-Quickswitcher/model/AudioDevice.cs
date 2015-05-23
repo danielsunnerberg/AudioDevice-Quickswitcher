ee186b03e511cfcb8660b61a5b016622b7f348ce
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2015-05-23 23:54:25 +0200

    Added AudioDevice-model which represents an OS audio device

---
+++ G930-Quickswitcher/model/AudioDevice.cs
@@ -0,0 +1,26 @@
+﻿namespace G930_Quickswitcher.model
+{
+    /// <summary>
+    /// Represents an audio device in the operating system
+    /// </summary>
+    class AudioDevice
+    {
+
+        /// <summary>
+        /// The devices internal id.
+        /// </summary>
+        public int Id { get; private set; }
+
+        /// <summary>
+        /// A string including the friendly name, driver name and such.
+        /// </summary>
+        public string Details { get; private set; }
+
+        public AudioDevice(int id, string details)
+        {
+            Id = id;
+            Details = details;
+        }
+
+    }
+}
